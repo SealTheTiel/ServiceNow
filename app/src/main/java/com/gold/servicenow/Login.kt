c052e497d6f1a6e83eabd3580bb52ed2ef7271cd
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-10-30 02:35:37 +0800

    add intents

--- app/src/main/java/com/gold/servicenow/Login.kt
+++ app/src/main/java/com/gold/servicenow/Login.kt
@@ -1,6 +1,10 @@
 package com.gold.servicenow
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +12,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login : ComponentActivity() {
+    private lateinit var loginButton: Button
+    private lateinit var googleButton: Button
+    private lateinit var signupButton: TextView
+    private lateinit var backButton: ImageButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +26,32 @@ class Login : ComponentActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        loginButton = findViewById(R.id.loginLoginButton)
+        googleButton = findViewById(R.id.loginGoogleButton)
+        signupButton = findViewById(R.id.loginSignup)
+        backButton = findViewById(R.id.loginBackButton)
+
+        // TEMPORARILY GO TO MAIN ACTIVITY
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        googleButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        signupButton.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        backButton.setOnClickListener {
+            finish()
         }
     }
 }
