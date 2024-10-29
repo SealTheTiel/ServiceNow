bd257f2ffa4f2e969bf0bedde5b16b7d28d39cdd
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-10-29 22:54:02 +0800

    add intents to login and register

--- app/src/main/java/com/gold/servicenow/Welcome.kt
+++ app/src/main/java/com/gold/servicenow/Welcome.kt
@@ -1,21 +1,40 @@
 package com.gold.servicenow
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.KeyEventDispatcher.Component
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Welcome : ComponentActivity() {
+    private lateinit var registerButton: Button
+    private lateinit var loginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        registerButton = findViewById(R.id.welcomeRegisterButton)
+        loginButton = findViewById(R.id.welcomeLoginButton)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+            finish()
         }
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
