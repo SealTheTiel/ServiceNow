3dad30e2f07740ebf47ad79dce773e7ec5cddf73
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-10-29 22:54:33 +0800

    convert to component activity

--- app/src/main/java/com/gold/servicenow/Login.kt
+++ app/src/main/java/com/gold/servicenow/Login.kt
@@ -1,12 +1,13 @@
 package com.gold.servicenow
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Login : AppCompatActivity() {
+class Login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/gold/servicenow/SplashScreenActivity.kt
+++ app/src/main/java/com/gold/servicenow/SplashScreenActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
+import android.widget.Button
 
 
 class SplashScreenActivity : ComponentActivity() {
