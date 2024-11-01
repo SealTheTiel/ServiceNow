ec207d9ece13fac70118ddc2c8750979f60c8a6e
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-11-01 15:42:10 +0800

    add dialog data

--- app/src/main/java/com/gold/servicenow/FoodViewHolder.kt
+++ app/src/main/java/com/gold/servicenow/FoodViewHolder.kt
@@ -14,9 +14,11 @@ class FoodViewHolder(itemView: View): ViewHolder(itemView) {
     private val name: TextView = itemView.findViewById(R.id.foodName)
     private val price: TextView = itemView.findViewById(R.id.foodPrice)
     private val button: CardView = itemView.findViewById(R.id.foodButton)
+    private lateinit var food: Food
 
 
     fun bindData(food: Food) {
+        this.food = food
         image.setImageResource(food.imageId)
         name.text = food.name
         price.text = "PHP " + food.price.toString()
@@ -37,5 +39,34 @@ class FoodViewHolder(itemView: View): ViewHolder(itemView) {
         dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
         dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
         dialog.window?.setGravity(android.view.Gravity.BOTTOM)
+
+        val dialogName: TextView = dialog.findViewById(R.id.addToCartName)
+        val dialogDetail1: TextView = dialog.findViewById(R.id.addToCartDetail1)
+        val dialogDetail2: TextView = dialog.findViewById(R.id.addToCartDetail2)
+        val dialogDescription1: TextView = dialog.findViewById(R.id.addToCartDescription1)
+        val dialogDescription2: TextView = dialog.findViewById(R.id.addToCartDescription2)
+        val dialogIncrement: Button = dialog.findViewById(R.id.addToCartIncrement)
+        val dialogDecrement: Button = dialog.findViewById(R.id.addToCartDecrement)
+        val dialogQuantity: TextView = dialog.findViewById(R.id.addToCartQuantity)
+        val dialogButton: Button = dialog.findViewById(R.id.addToCartButton)
+
+
+        dialogName.text = food.name
+        dialogDetail1.text = food.detail1
+        dialogDetail2.text = food.detail2
+        dialogDescription1.text = food.restaurant
+        dialogDescription2.text = food.description
+        dialogIncrement.setOnClickListener{
+            var quantity = dialogQuantity.text.toString().toInt()
+            quantity++
+            dialogQuantity.text = quantity.toString()
+        }
+        dialogDecrement.setOnClickListener{
+            var quantity = dialogQuantity.text.toString().toInt()
+            if(quantity > 1) {
+                quantity--
+                dialogQuantity.text = quantity.toString()
+            }
+        }
     }
 }
