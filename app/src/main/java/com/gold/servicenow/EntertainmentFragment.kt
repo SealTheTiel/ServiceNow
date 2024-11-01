190c23288660108173cc69f8d61f62a00424e616
Daphne Go <daphnejanelyngo@gmail.com>
2024-11-01 13:34:40 +0800

    Edit Cart Functionality

--- app/src/main/java/com/gold/servicenow/EntertainmentFragment.kt
+++ app/src/main/java/com/gold/servicenow/EntertainmentFragment.kt
@@ -1,14 +1,17 @@
 package com.gold.servicenow
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class EntertainmentFragment : Fragment() {
+    private lateinit var cart: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +21,20 @@ class EntertainmentFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment and get the view object
+        val view = inflater.inflate(R.layout.fragment_entertainment, container, false)
 
-        return inflater.inflate(R.layout.fragment_entertainment, container, false)
+        // Initialize cart button
+        cart = view.findViewById(R.id.cartLeisure)
+        cart.setOnClickListener {
+            // Navigate to AddtoCart activity
+            val intent = Intent(context, AddtoCart::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
