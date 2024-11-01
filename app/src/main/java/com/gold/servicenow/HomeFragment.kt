1570946f6424d67ec21a53a4f5f27c1674d1ef5c
Daphne Go <daphnejanelyngo@gmail.com>
2024-11-01 13:06:02 +0800

    Data Generator Edits

--- app/src/main/java/com/gold/servicenow/HomeFragment.kt
+++ app/src/main/java/com/gold/servicenow/HomeFragment.kt
@@ -1,23 +1,49 @@
 package com.gold.servicenow
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var medicineButton: CardView
+    private lateinit var foodButton: CardView
+    private lateinit var entertainmentButton: CardView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Initialize buttons
+        medicineButton = view.findViewById(R.id.medicine_button)
+        foodButton = view.findViewById(R.id.food_button)
+        entertainmentButton = view.findViewById(R.id.entertainment_button)
+
+        // Set click listeners for each button
+        medicineButton.setOnClickListener {
+            replaceFragment(MedicineFragment())
+        }
+
+        foodButton.setOnClickListener {
+            replaceFragment(FoodFragment())
+        }
+
+        entertainmentButton.setOnClickListener {
+            replaceFragment(EntertainmentFragment())
+        }
+
+        return view
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        var fragmentTransition = parentFragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.frame, fragment)
+        fragmentTransition.addToBackStack(null)
+        fragmentTransition.commit()
     }
 }
