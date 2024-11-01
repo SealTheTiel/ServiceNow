4ca430c3a87ce8f103ed2c4541ab6b36490e9b5e
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-11-01 15:10:45 +0800

    edit clickdetector for dialog

--- app/src/main/java/com/gold/servicenow/FoodViewHolder.kt
+++ app/src/main/java/com/gold/servicenow/FoodViewHolder.kt
@@ -1,6 +1,8 @@
 package com.gold.servicenow
 
+import android.app.Dialog
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -13,10 +15,27 @@ class FoodViewHolder(itemView: View): ViewHolder(itemView) {
     private val price: TextView = itemView.findViewById(R.id.foodPrice)
     private val button: CardView = itemView.findViewById(R.id.foodButton)
 
+
     fun bindData(food: Food) {
         image.setImageResource(food.imageId)
         name.text = food.name
         price.text = "PHP " + food.price.toString()
     }
 
+    init {
+        itemView.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(itemView.context)
+        dialog.requestWindowFeature(android.view.Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.activity_view_details)
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(android.view.Gravity.BOTTOM)
+    }
 }
