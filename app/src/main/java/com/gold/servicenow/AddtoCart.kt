d2daddf2e16e7ec15cf02bb961c0918ebd5cd539
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-11-01 19:21:27 +0800

    fix cart layout

--- app/src/main/java/com/gold/servicenow/AddtoCart.kt
+++ app/src/main/java/com/gold/servicenow/AddtoCart.kt
@@ -3,14 +3,11 @@ package com.gold.servicenow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class AddtoCart : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_addto_cart)
+        setContentView(R.layout.activity_cart)
     }
 }
