927d4a34e307031c5ca28da15073ae16aa8dc19b
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-11-19 00:07:04 +0800

    modify image to use url instead of local image resource modify id name

--- app/src/main/java/com/gold/servicenow/entertainment/Entertainment.kt
+++ app/src/main/java/com/gold/servicenow/entertainment/Entertainment.kt
@@ -1,9 +1,9 @@
 package com.gold.servicenow.entertainment
 
-class Entertainment (entertainment_id:Int, imageId: Int, name: String, price: Float, description: String, location: String, contact: String, detail1:String, detail2: String) {
-    var entertainment_id: Int = 0
+class Entertainment (id:Int, imageUrl: String, name: String, price: Float, description: String, location: String, contact: String, detail1:String, detail2: String) {
+    var id: Int = 0
         private set
-    var imageId = imageId
+    var imageUrl = imageUrl
         private set
 
     var name = name
