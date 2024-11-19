1d788c8b3a9683bd3f120ada88e8a50ee4c6fdc6
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-11-19 21:19:41 +0800

    readd profile activity

---
+++ app/src/main/java/com/gold/servicenow/profile/ProfileActivity.kt
@@ -0,0 +1,116 @@
+package com.gold.servicenow.profile
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.*
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.gold.servicenow.R
+import com.gold.servicenow.databinding.ActivityMainBinding
+import com.gold.servicenow.entertainment.EntertainmentFragment
+import com.gold.servicenow.food.FoodFragment
+import com.gold.servicenow.medicine.MedicineFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.gold.servicenow.database.DatabaseHandler
+import com.google.firebase.FirebaseApp
+
+
+class ProfileActivity: ComponentActivity() {
+    private lateinit var nameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var contactEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var updateButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
+//            v.updatePadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            WindowInsetsCompat.CONSUMED
+//        }
+        setContentView(R.layout.activity_profile)
+
+        var nameEdited: Boolean = false
+        var emailEdited: Boolean = false
+        var contactEdited: Boolean = false
+        var passwordEdited: Boolean = false
+
+
+        nameEditText = findViewById(R.id.profileNameInput)
+        emailEditText = findViewById(R.id.profileEmailInput)
+        contactEditText = findViewById(R.id.profileNumberInput)
+        passwordEditText = findViewById(R.id.profilePasswordInput)
+        updateButton = findViewById(R.id.profileUpdateButton)
+
+        println(CurrentProfile.profile?.name)
+        println(CurrentProfile.profile?.email)
+        println(CurrentProfile.profile?.contact)
+        println(CurrentProfile.profile?.password)
+
+
+        nameEditText.setText(CurrentProfile.profile?.name)
+        emailEditText.setText(CurrentProfile.profile?.email)
+        passwordEditText.setText(CurrentProfile.profile?.password)
+        contactEditText.setText(CurrentProfile.profile?.contact)
+
+        updateButton.isEnabled = false
+        updateButton.backgroundTintList = getColorStateList(R.color.light_gray)
+        nameEditText.addTextChangedListener {
+            nameEdited = nameEditText.text.toString() != CurrentProfile.profile?.name
+            if (nameEdited || emailEdited || contactEdited || passwordEdited) { setButtonActivated(updateButton, true) }
+            else { setButtonActivated(updateButton, false) }
+        }
+
+        emailEditText.addTextChangedListener {
+            emailEdited = emailEditText.text.toString() != CurrentProfile.profile?.email
+            if (nameEdited || emailEdited || contactEdited || passwordEdited) { setButtonActivated(updateButton, true) }
+            else { setButtonActivated(updateButton, false) }
+        }
+
+        contactEditText.addTextChangedListener {
+            contactEdited = contactEditText.text.toString() != CurrentProfile.profile?.contact
+            if (nameEdited || emailEdited || contactEdited || passwordEdited) { setButtonActivated(updateButton, true) }
+            else { setButtonActivated(updateButton, false) }
+        }
+
+        passwordEditText.addTextChangedListener {
+            passwordEdited = passwordEditText.text.toString() != CurrentProfile.profile?.password
+            if (nameEdited || emailEdited || contactEdited || passwordEdited) { setButtonActivated(updateButton, true) }
+            else { setButtonActivated(updateButton, false) }
+        }
+
+        updateButton.setOnClickListener {
+            val name = nameEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val contact = contactEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            val newProfile = Profile(name, email, contact, password)
+            CurrentProfile.update(newProfile,
+                onSuccess = {
+                    println("[Profile]: Profile updated successfully.")
+                    finish()
+                },
+                onFailure = {
+                    println("[Profile]: Profile update failed.")
+                })
+            finish()
+        }
+
+    }
+    fun setButtonActivated(button: Button, status: Boolean) {
+        if (status) {
+            button.isEnabled = true
+            button.backgroundTintList = getColorStateList(R.color.primary_blue)
+        } else {
+            button.isEnabled = false
+            button.backgroundTintList = getColorStateList(R.color.light_gray)
+        }
+    }
+}
