88e4ee0076374cb3dfd5fcf1474043a4d075536d
Bentley Lu <119946617+Boontly@users.noreply.github.com>
2024-11-19 15:32:22 +0800

    created dummy data

--- app/src/main/java/com/gold/servicenow/DataGenerator.kt
+++ app/src/main/java/com/gold/servicenow/DataGenerator.kt
@@ -76,6 +76,96 @@ class DataGenerator {
             "Restaurant",
             "Ingredients"
         )
+        private val foodname8: Food = Food(
+            8,
+            "",
+            "Tinolang Manok",
+            55.0f,
+            "Home-Cooked Meals",
+            "Chicken, Papaya, Ginger, Spinach",
+            "Restaurant",
+            "Ingredients"
+        )
+        private val foodname9: Food = Food(
+            9,
+            "",
+            "Ginataang Gulay",
+            50.0f,
+            "Lutong Bahay",
+            "Vegetables, Coconut Milk, Shrimp",
+            "Restaurant",
+            "Ingredients"
+        )
+        private val foodname10: Food = Food(
+            10,
+            "",
+            "Pinakbet",
+            45.0f,
+            "Ilocos Cuisine",
+            "Vegetables, Bagoong, Pork",
+            "Restaurant",
+            "Ingredients"
+        )
+        private val foodname11: Food = Food(
+            11,
+            "",
+            "Bulalo",
+            100.0f,
+            "Tagaytay Eats",
+            "Beef Shank, Corn, Vegetables",
+            "Restaurant",
+            "Ingredients"
+        )
+        private val foodname12: Food = Food(
+            12,
+            "",
+            "Siomai",
+            35.0f,
+            "Dimsum House",
+            "Ground Pork, Shrimp, Carrots",
+            "Snack",
+            "Ingredients"
+        )
+        private val foodname13: Food = Food(
+            13,
+            "",
+            "Paksiw na Bangus",
+            50.0f,
+            "Lutong Isda",
+            "Milkfish, Vinegar, Garlic, Onion",
+            "Restaurant",
+            "Ingredients"
+        )
+        private val foodname14: Food = Food(
+            14,
+            "",
+            "Monggo Guisado",
+            40.0f,
+            "Healthy Filipino Meals",
+            "Mung Beans, Pork, Spinach",
+            "Restaurant",
+            "Ingredients"
+        )
+        private val foodname15: Food = Food(
+            15,
+            "",
+            "Nilagang Baka",
+            90.0f,
+            "Comfort Food",
+            "Beef, Potatoes, Cabbage, Corn",
+            "Restaurant",
+            "Ingredients"
+        )
+        private val foodname16: Food = Food(
+            16,
+            "",
+            "Lumpiang Sariwa",
+            50.0f,
+            "Filipino Favorites",
+            "Vegetables, Wrapper, Peanut Sauce",
+            "Restaurant",
+            "Ingredients"
+        )
 
         fun getFoodName(): ArrayList<Food> {
             return arrayListOf(foodname1, foodname2, foodname3, foodname4, foodname5, foodname6, foodname7)
@@ -146,6 +236,116 @@ class DataGenerator {
             "1 tablet daily",
             "Side Effects",
             "Nausea, Diarrhea"
+        )
+        private val medicine7: Medicine = Medicine(
+            7,
+            "",
+            "Losartan",
+            70.0f,
+            "High Blood Pressure",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Dizziness, Fatigue"
+        )
+        private val medicine8: Medicine = Medicine(
+            8,
+            "",
+            "Amlodipine",
+            80.0f,
+            "Hypertension",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Swelling, Dizziness"
+        )
+        private val medicine9: Medicine = Medicine(
+            9,
+            "",
+            "Aspirin",
+            90.0f,
+            "Pain, Blood Thinner",
+            "Dosage",
+            "1 tablet every 4-6 hours as needed",
+            "Side Effects",
+            "Stomach Upset, Bleeding"
+        )
+        private val medicine10: Medicine = Medicine(
+            10,
+            "",
+            "Captopril",
+            100.0f,
+            "High Blood Pressure",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Cough, Dizziness"
+        )
+        private val medicine11: Medicine = Medicine(
+            11,
+            "",
+            "Metoprolol",
+            110.0f,
+            "Heart Problems",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Tiredness, Low Heart Rate"
+        )
+        private val medicine12: Medicine = Medicine(
+            12,
+            "",
+            "Clopidogrel",
+            120.0f,
+            "Blood Clot Prevention",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Bruising, Bleeding"
+        )
+        private val medicine13: Medicine = Medicine(
+            13,
+            "",
+            "Furosemide",
+            130.0f,
+            "Fluid Retention, High Blood Pressure",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Frequent Urination, Dizziness"
+        )
+        private val medicine14: Medicine = Medicine(
+            14,
+            "",
+            "Allopurinol",
+            140.0f,
+            "Gout",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Rash, Nausea"
+        )
+        private val medicine15: Medicine = Medicine(
+            15,
+            "",
+            "Hydrochlorothiazide",
+            150.0f,
+            "High Blood Pressure, Fluid Retention",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Low Potassium, Dizziness"
+        )
+        private val medicine16: Medicine = Medicine(
+            16,
+            "",
+            "Atorvastatin",
+            160.0f,
+            "High Cholesterol",
+            "Dosage",
+            "1 tablet daily",
+            "Side Effects",
+            "Muscle Pain, Stomach Upset"
         )
 
         fun getMedicine(): ArrayList<Medicine> {
@@ -218,6 +418,29 @@ class DataGenerator {
             "Location",
             "Contact"
         )
+        private val entertainment7: Entertainment = Entertainment(
+            7,
+            "",
+            "Movie Theater",
+            120.0f,
+            "Watching Movies",
+            "Cebu City",
+            "09123456789",
+            "Location",
+            "Contact"
+        )
+        private val entertainment8: Entertainment = Entertainment(
+            8,
+            "",
+            "Bowling",
+            180.0f,
+            "Bowling Games",
+            "Davao City",
+            "09123456789",
+            "Location",
+            "Contact"
+        )
+
 
         fun getLeisure(): ArrayList<Entertainment> {
             return arrayListOf(entertainment1, entertainment2, entertainment3, entertainment4, entertainment5, entertainment6)
