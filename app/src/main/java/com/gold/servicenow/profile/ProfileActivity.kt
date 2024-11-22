160a66c53ed4ecd74f24a840220d28802e57f5bb
SealTheTiel <sealtieldy74@gmail.com>
2024-11-22 13:43:45 +0800

    refactor to use binding instead of find by id

--- app/src/main/java/com/gold/servicenow/profile/ProfileActivity.kt
+++ app/src/main/java/com/gold/servicenow/profile/ProfileActivity.kt
@@ -4,35 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.*
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.gold.servicenow.InputValidator
 import com.gold.servicenow.R
-import com.gold.servicenow.databinding.ActivityMainBinding
-import com.gold.servicenow.entertainment.EntertainmentFragment
-import com.gold.servicenow.food.FoodFragment
-import com.gold.servicenow.medicine.MedicineFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.gold.servicenow.database.DatabaseHandler
+import com.gold.servicenow.databinding.ActivityProfileBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.FirebaseApp
 
 
 class ProfileActivity: ComponentActivity() {
-    private lateinit var nameEditLayout: TextInputLayout
-    private lateinit var nameEditText: TextInputEditText
-    private lateinit var emailEditLayout: TextInputLayout
-    private lateinit var emailEditText: TextInputEditText
-    private lateinit var passwordEditLayout: TextInputLayout
-    private lateinit var passwordEditText: TextInputEditText
-    private lateinit var contactEditLayout: TextInputLayout
-    private lateinit var contactEditText: TextInputEditText
-    private lateinit var updateButton: Button
+    private lateinit var binding: ActivityProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,68 +36,53 @@ class ProfileActivity: ComponentActivity() {
 //            v.updatePadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            WindowInsetsCompat.CONSUMED
 //        }
-        setContentView(R.layout.activity_profile)
-
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         var nameEdited: Boolean = false
         var emailEdited: Boolean = false
         var contactEdited: Boolean = false
         var passwordEdited: Boolean = false
+        var imageEdited: Boolean = false
+
+        binding.profileNameInput.setText(CurrentProfile.profile?.name)
+        binding.profileEmailInput.setText(CurrentProfile.profile?.email)
+        binding.profilePasswordInput.setText(CurrentProfile.profile?.password)
+        binding.profileNumberInput.setText(CurrentProfile.profile?.contact)
+
+        setButtonActivated(binding.profileUpdateButton, false)
 
 
-        nameEditText = findViewById(R.id.profileNameInput)
-        emailEditText = findViewById(R.id.profileEmailInput)
-        contactEditText = findViewById(R.id.profileNumberInput)
-        passwordEditText = findViewById(R.id.profilePasswordInput)
-        nameEditLayout = findViewById(R.id.profileName)
-        emailEditLayout = findViewById(R.id.profileEmail)
-        passwordEditLayout = findViewById(R.id.profilePassword)
-        contactEditLayout = findViewById(R.id.profileNumber)
-        updateButton = findViewById(R.id.profileUpdateButton)
-
-        println(CurrentProfile.profile?.name)
-        println(CurrentProfile.profile?.email)
-        println(CurrentProfile.profile?.contact)
-        println(CurrentProfile.profile?.password)
-
-
-        nameEditText.setText(CurrentProfile.profile?.name)
-        emailEditText.setText(CurrentProfile.profile?.email)
-        passwordEditText.setText(CurrentProfile.profile?.password)
-        contactEditText.setText(CurrentProfile.profile?.contact)
-
-        setButtonActivated(updateButton, false)
-
-        nameEditText.addTextChangedListener {
-            nameEdited = nameEditText.text.toString() != CurrentProfile.profile?.name
-            if (nameEdited || emailEdited || contactEdited || passwordEdited) { setButtonActivated(updateButton, true) }
-            else { setButtonActivated(updateButton, false) }
+        binding.profileNameInput.addTextChangedListener {
+            nameEdited = binding.profileNameInput.text.toString() != CurrentProfile.profile?.name
+            if (nameEdited || emailEdited || contactEdited || passwordEdited || imageEdited) { setButtonActivated(binding.profileUpdateButton, true) }
+            else { setButtonActivated(binding.profileUpdateButton, false) }
         }
 
-        emailEditText.setOnFocusChangeListener { view, focused ->
+        binding.profileEmailInput.setOnFocusChangeListener { view, focused ->
             if (focused) return@setOnFocusChangeListener
-            emailEditLayout.helperText = InputValidator.validateEmail(emailEditText.text.toString())
+            binding.profileEmail.helperText = InputValidator.validateEmail(binding.profileEmailInput.text.toString())
         }
 
-        emailEditText.addTextChangedListener {
-            emailEdited = emailEditText.text.toString() != CurrentProfile.profile?.email
-            if (nameEdited || emailEdited || contactEdited || passwordEdited) { setButtonActivated(updateButton, true) }
-            else { setButtonActivated(updateButton, false) }
+        binding.profileEmailInput.addTextChangedListener {
+            emailEdited = binding.profileEmailInput.text.toString() != CurrentProfile.profile?.email
+            if (nameEdited || emailEdited || contactEdited || passwordEdited || imageEdited) { setButtonActivated(binding.profileUpdateButton, true) }
+            else { setButtonActivated(binding.profileUpdateButton, false) }
         }
 
-        contactEditText.addTextChangedListener {
-            contactEdited = contactEditText.text.toString() != CurrentProfile.profile?.contact
-            if (nameEdited || emailEdited || contactEdited || passwordEdited) { setButtonActivated(updateButton, true) }
-            else { setButtonActivated(updateButton, false) }
+        binding.profileNumberInput.addTextChangedListener {
+            contactEdited = binding.profileNumberInput.text.toString() != CurrentProfile.profile?.contact
+            if (nameEdited || emailEdited || contactEdited || passwordEdited || imageEdited) { setButtonActivated(binding.profileUpdateButton, true) }
+            else { setButtonActivated(binding.profileUpdateButton, false) }
         }
 
-        passwordEditText.addTextChangedListener {
-            passwordEdited = passwordEditText.text.toString() != CurrentProfile.profile?.password
-            if (nameEdited || emailEdited || contactEdited || passwordEdited) { setButtonActivated(updateButton, true) }
-            else { setButtonActivated(updateButton, false) }
+        binding.profilePasswordInput.addTextChangedListener {
+            passwordEdited = binding.profilePasswordInput.text.toString() != CurrentProfile.profile?.password
+            if (nameEdited || emailEdited || contactEdited || passwordEdited || imageEdited) { setButtonActivated(binding.profileUpdateButton, true) }
+            else { setButtonActivated(binding.profileUpdateButton, false) }
         }
-        passwordEditText.setOnFocusChangeListener { view, focused ->
+        binding.profilePasswordInput.setOnFocusChangeListener { view, focused ->
             if (focused) return@setOnFocusChangeListener
-            passwordEditLayout.helperText = InputValidator.validatePassword(passwordEditText.text.toString())
+            binding.profilePassword.helperText = InputValidator.validatePassword(binding.profilePasswordInput.text.toString())
         }
 
         updateButton.setOnClickListener {
@@ -124,6 +103,7 @@ class ProfileActivity: ComponentActivity() {
         }
 
     }
+
     private fun setButtonActivated(button: Button, status: Boolean) {
         if (status) {
             button.isEnabled = true
