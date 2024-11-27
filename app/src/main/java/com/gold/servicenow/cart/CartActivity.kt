91b1c556f460a1d3e243a9c97f4862f70cd5328b
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2024-11-28 01:18:14 +0800

    removed unused imports

--- app/src/main/java/com/gold/servicenow/cart/CartActivity.kt
+++ app/src/main/java/com/gold/servicenow/cart/CartActivity.kt
@@ -1,7 +1,5 @@
 package com.gold.servicenow.cart
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
